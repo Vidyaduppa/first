97216d22279ad01cc8e61183629d46deeb548374
vidya <n190062@rguktn.ac.in>
2023-07-15 01:41:55 +0530

    committed

---
+++ ds2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	struct node
+	{
+		int data;
+		struct node*next;
+	};
+	struct student;
+	int id;
+	char name[20];
+	char gender;
+	struct node* next;
+	}
+
